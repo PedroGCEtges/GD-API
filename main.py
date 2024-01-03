6822162e4ace26bf14dd956e7e22f1c7007c0b21
PedroGCEtges <pedroetges11@gmail.com>
2024-01-03 14:44:13 -0300

    novo teste

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import getenv
 from fastapi import Depends, FastAPI, HTTPException, status, Request
 from routes import route
 from fastapi.templating import Jinja2Templates
@@ -17,3 +18,7 @@ def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 app.include_router(route, prefix="")
+
+if __name__ == "__main__":
+    port = int(getenv("PORT", 3000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
